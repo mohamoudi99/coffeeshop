4fdde231cc34f4afedeea485a809f7a289bb49ed
Mohamad amoudi <96519017+mohamoudi99@users.noreply.github.com>
2024-01-01 15:35:45 +0200

    last

--- coffee_frontend/lib/main.dart
+++ coffee_frontend/lib/main.dart
@@ -1,23 +1,36 @@
+import 'package:coffee_frontend/SignUp.dart';
+import 'package:coffee_frontend/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app_state.dart';
 import 'HomePage.dart'; // Make sure to import your HomePage.dart file
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  runApp(MyApp(token: prefs.getString('token')));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String? token;
+
+  const MyApp({
+    Key? key,
+    this.token,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => AppState(),
-      child: const MaterialApp(
+      child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: HomePage(),
       ),
+      home:(JwtDecoder.isExpired(token) == false)?Dashboard(token: token):SignUpPage();
     );
   }
 }
